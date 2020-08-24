10098f589229792d618f9cc933b987bc4a03ed66
yuya.nakagawa <yuye4049@gmail.com>
2020-08-24 23:54:09 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,19 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user_logged_in
     def index
-        @tasks = Task.order(id: :desc).page(params[:page]).per(10)
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(10)
     end
     
     def show
     end
     
     def new
-        @task = Task.new
+        @task = current_user.tasks.build
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = 'タスクを追加しました'
@@ -42,14 +42,17 @@ class TasksController < ApplicationController
         @task.destroy
         
         flash[:success] = 'タスクは削除されました'
-        redirect_to tasks_url
+        redirect_back(fallback_location: root_path)
     end
     
     private
     
     # 共通化
-    def set_task
-        @task = Task.find(params[:id])
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+          redirect_to root_url
+        end
     end
     
     # Strong Parameter
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
     root to: 'tasks#index'
     
     resources :tasks
